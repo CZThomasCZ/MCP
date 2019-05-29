e08a002089ae65787f01c3f197082b47cce81a8f
Matthew <CZThomasCZ@users.noreply.github.com>
2019-05-29 17:03:10 +0200

    Initial Upload, v1.0.1.7

---
+++ MCP/EventHandler.cs
@@ -0,0 +1,122 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+using Smod2.API;
+using Smod2.EventHandlers;
+using System;
+using Smod2.Config;
+using System.Collections.Generic;
+using System.Timers;
+using gmLCZEscape;
+using gmDBoiz;
+using gmNormal;
+using gmMTF;
+
+namespace MCP
+{
+    public class PluginHandler : IEventHandlerRoundStart, IEventHandlerPlayerDie
+    {
+        private readonly string broadcast_error = "Config error";
+        private readonly string cassie_error = "CORRUPTED CODE PITCH_1,5 CORRUPTED CODE PITCH_2 CORRUPTED CODE PITCH_2,5 CORRUPTED CODE PITCH_3 CORRUPTED CODE";
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {   //Broadcast current gamemode + announce it using C.A.S.S.I.E.
+            string gamemode = GamemodeManager.GamemodeManager.GetCurrentName();
+            string broadcast = "ERROR";
+            string cassie = "ERROR";
+
+            if (gamemode == "LCZ Escape")
+            {
+                broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_lcz_broadcast", broadcast_error, false);
+                cassie = ConfigManager.Manager.Config.GetStringValue("mcp_lcz_cassie", cassie_error, false);
+            }
+            else if (gamemode == "Normal gamemode")
+            {
+                broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_normal_broadcast", broadcast_error, false);
+                cassie = ConfigManager.Manager.Config.GetStringValue("mcp_normal_cassie", cassie_error, false);
+            }
+            else if (gamemode == "D-Boiz escape")
+            {
+                broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_dboiz_broadcast", broadcast_error, false);
+                cassie = ConfigManager.Manager.Config.GetStringValue("mcp_dboiz_cassie", cassie_error, false);
+            }
+            else if (gamemode == "MTF Recontainment")
+            {
+                broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_mtf_broadcast", broadcast_error, false);
+                cassie = ConfigManager.Manager.Config.GetStringValue("mcp_mtf_cassie", cassie_error, false);
+                
+                // Respawn guards as a MTF Unit
+                int MTFs = 0;
+                foreach (Player player in PluginManager.Manager.Server.GetPlayers(Role.FACILITY_GUARD))
+                {
+                    if (MTFs == 0) player.ChangeRole(Role.NTF_COMMANDER);
+                    else if (MTFs < 3) player.ChangeRole(Role.NTF_LIEUTENANT);
+                    else player.ChangeRole(Role.NTF_CADET);
+                    MTFs++;
+                }
+            }
+            else if (gamemode == "Facility Raid")
+            {
+                int spectators = 0;
+                int players = 0;
+                bool enable = false;
+                bool SH = false;
+                foreach (Player player in PluginManager.Manager.Server.GetPlayers())
+                {
+                    if (player.TeamRole.Role == Role.SPECTATOR) spectators++;
+                    players++;
+                }
+                if (players > 5) enable = true;
+                if (enable == true)
+                {   //Some chance to enable Serpent's Hand? 
+                    Random random;
+                    random = new Random();
+                    double Chance = random.NextDouble(); // Gets value between 0.0 and 1.0
+                    if (Chance < 0.5) SH = true;
+
+                }
+
+                foreach (Player player in PluginManager.Manager.Server.GetPlayers(Role.SPECTATOR))
+                {
+                    if (SH == true) SerpentsHand.SHPlugin.SpawnPlayer(player);
+                    else player.ChangeRole(Role.CHAOS_INSURGENCY);
+                }
+                if (SH == false)
+                {
+                    broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_raid_broadcast_ci", broadcast_error, false);
+                    cassie = ConfigManager.Manager.Config.GetStringValue("mcp_raid_cassie_ci", cassie_error, false);
+                } else
+                {
+                    broadcast = ConfigManager.Manager.Config.GetStringValue("mcp_raid_broadcast_sh", broadcast_error, false);
+                    cassie = ConfigManager.Manager.Config.GetStringValue("mcp_raid_cassie_sh", cassie_error, false);
+                }
+
+            }
+
+            PluginManager.Manager.Server.Map.Broadcast(10, "Current gamemode: " + broadcast, false);
+            PluginManager.Manager.Server.Map.AnnounceCustomMessage(cassie);
+        }
+
+        public void OnPlayerDie(PlayerDeathEvent ev)
+        {   //Revive player if killed by SCP-049-2
+            if(ev.Killer.TeamRole.Role == Role.SCP_049_2)
+            {
+                var deathPos = ev.Player.GetPosition();
+
+                var respawnTimer = new Timer
+                {
+                    AutoReset = false,
+                    Enabled = true,
+                    Interval = 2000
+                };
+                respawnTimer.Elapsed += delegate
+                {
+                    ev.Player.ChangeRole(Role.SCP_049_2);
+                    var health = ev.Player.GetHealth()/2;
+                    ev.Player.Damage(health, DamageType.NONE);
+                    ev.Player.Teleport(deathPos);
+                };
+            }
+        }
+    }
+}
---
+++ MCP/MCP.cs
@@ -0,0 +1,203 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+using Smod2.API;
+using Smod2.EventHandlers;
+using System;
+using Smod2.Config;
+using System.Collections.Generic;
+
+namespace MCP
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "Matthew's Content Pack",
+        description = "Main plugin for the rest of my customizations",
+        id = "matthew.main",
+        configPrefix = "mcp",
+        version = "1.1.0",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class MCP : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+        public override void OnEnable()
+        {
+            Info("Matthew's Content Pack loaded!");
+        }
+        public override void Register()
+        {
+            AddEventHandlers(new PluginHandler());
+        }
+    }
+}
+
+namespace gmLCZEscape
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "LCZ Escape",
+        description = "Light Containment Zone escape",
+        id = "matthew.gamemode.lcz_escape",
+        version = "1.1.0",
+        configPrefix = "mcp",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class GMlczEscape : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+            //Info("Light Containment Zone escape enabled.");
+        }
+
+        public override void Register()
+        {
+            string queue = ConfigManager.Manager.Config.GetStringValue("mcp_lcz_queue", "30434044334044404444", false);
+            GamemodeManager.GamemodeManager.RegisterMode(this, queue);
+        }
+    }
+}
+
+namespace gmNormal
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "Normal gamemode",
+        description = "Normal gamemode",
+        id = "matthew.gamemode.normal",
+        version = "1.1.0",
+        configPrefix = "mcp",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class GMNormal : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+            //Info("Normal gamemode enabled.");
+        }
+
+        public override void Register()
+        {
+            string queue = ConfigManager.Manager.Config.GetStringValue("mcp_normal_queue", "40143140314414041340", false);
+            GamemodeManager.GamemodeManager.RegisterMode(this, queue);
+        }
+    }
+}
+
+namespace gmDBoiz
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "D-Boiz escape",
+        description = "D-Boiz escape",
+        id = "matthew.gamemode.dboiz",
+        version = "1.1.0",
+        configPrefix = "mcp",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class GMDBoiz : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+            //Info("D-Boiz escape enabled.");
+        }
+
+        public override void Register()
+        {
+            string queue = ConfigManager.Manager.Config.GetStringValue("mcp_dboiz_queue", "40444440444044404444", false);
+            GamemodeManager.GamemodeManager.RegisterMode(this, queue);
+        }
+    }
+}
+
+namespace gmMTF
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "MTF Recontainment",
+        description = "MTF Recontainment",
+        id = "matthew.gamemode.mtf",
+        version = "1.1.0",
+        configPrefix = "mcp",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class GMMTF : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+            //Info("MTF Recontainment enabled.");
+        }
+
+        public override void Register()
+        {
+            string queue = ConfigManager.Manager.Config.GetStringValue("mcp_mtf_queue", "10111110110111101111", false);
+            GamemodeManager.GamemodeManager.RegisterMode(this, queue);
+        }
+    }
+}
+
+namespace gmRaid
+{
+    [PluginDetails(
+        author = "Matthew05",
+        name = "Facility Raid",
+        description = "Facility Raid",
+        id = "matthew.gamemode.raid",
+        version = "1.1.0",
+        configPrefix = "mcp",
+        SmodMajor = 3,
+        SmodMinor = 0,
+        SmodRevision = 0
+        )]
+    public class GMCI : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+            //Info("MTF Recontainment enabled.");
+        }
+
+        public override void Register()
+        {
+            string queue = ConfigManager.Manager.Config.GetStringValue("mcp_raid_queue", "40543540354454045340", false);
+            GamemodeManager.GamemodeManager.RegisterMode(this, queue);
+        }
+    }
+}
